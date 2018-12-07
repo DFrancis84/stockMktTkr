2e3c49d9d27f25d58447eeeba00c80d8dcac0dfb
Devin Francis <devin.francis@eltoro.com>
2018-12-07 16:07:33 -0500

    changed root from aa to av since site is called Alpha Vantage. Also updated comments to reflect correct name

--- quote/quote.go
+++ quote/quote.go
@@ -9,16 +9,16 @@ import (
 )
 
 const (
-	aaRoot = "https://www.alphavantage.co/query"
+	avRoot = "https://www.alphavantage.co/query"
 )
 
-// API provides methods for interacting with AA
+// API provides methods for interacting with Alpha Vantage
 type API struct {
 	symbols []string
 	key     string
 }
 
-// New returns reference to AA API
+// New returns reference to Alpha Vantage API
 func New(symbols []string, key string) *API {
 	return &API{
 		symbols: symbols,
@@ -26,7 +26,7 @@ func New(symbols []string, key string) *API {
 	}
 }
 
-// GetImmediateStockQuote sends request to AA and returns stock quotes based on stored symbols in config file.
+// GetImmediateStockQuote sends request to Alpha Vantage and returns stock quotes based on stored symbols in config file.
 func GetImmediateStockQuote(symbols []string, key string) {
 	client := http.DefaultClient
 	quote := API{
@@ -35,7 +35,7 @@ func GetImmediateStockQuote(symbols []string, key string) {
 	}
 
 	for _, symbol := range symbols {
-		getQuoteURI := fmt.Sprintf("%v?function=GLOBAL_QUOTE&symbol=%v&apikey=%v", aaRoot, symbol, quote.key)
+		getQuoteURI := fmt.Sprintf("%v?function=GLOBAL_QUOTE&symbol=%v&apikey=%v", avRoot, symbol, quote.key)
 		request, err := createDefaultRequest(http.MethodGet, getQuoteURI)
 		if err != nil {
 			log.Fatal(err)
@@ -54,12 +54,11 @@ func GetImmediateStockQuote(symbols []string, key string) {
 		sB := string(body)
 		fmt.Printf(sB)
 
-		// AA allows 5 request per minute, this timer will limit to 5 requests per minute if over 5 symbols
+		// Alpha Vantage allows 5 request per minute, this timer will limit to 5 requests per minute if over 5 symbols
 		if len(symbols) > 5 {
 			time.Sleep(12 * time.Second)
 		}
 	}
-
 }
 
 func createDefaultRequest(method, uri string) (*http.Request, error) {
